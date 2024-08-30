9786bcf7dbebb54a03ddf9128b942ec5372b8c04
Nicholas Homayouni <nicholashomayouni@Nicholass-Mac-Studio.local>
2024-08-30 13:27:19 -0400

    first commit

---
+++ src/main/java/com/CircuitFlux/ecomm_app_backend/EcommAppBackendApplication.java
@@ -0,0 +1,14 @@
+package com.CircuitFlux.ecomm_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommAppBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EcommAppBackendApplication.class, args);
+	}
+
+}
