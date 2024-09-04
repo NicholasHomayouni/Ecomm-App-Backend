a3d144248dda7e8a5ea292718539f1339d0b4309
Nicholas Homayouni <nickh1997@outlook.com>
2024-09-03 20:45:02 -0400

    added getter and setter methods to product class for use

--- src/main/java/com/CircuitFlux/ecomm_app_backend/entity/Product.java
+++ src/main/java/com/CircuitFlux/ecomm_app_backend/entity/Product.java
@@ -54,4 +54,28 @@ public class Product {
     public void setName(String name) {
         this.name = name;
     }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
